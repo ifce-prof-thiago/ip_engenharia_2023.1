0e2ed8a2698c81f9f299f550f22a6f09ffcc4f55
Thiago Bandeira <thiago@lar.ifce.edu.br>
2023-06-01 10:44:06 -0300

    10 e 11

---
+++ 10_todo.cpp
@@ -0,0 +1,6 @@
+/*
+    Escreva um programa que calcule o perímetro e a área
+    de um triângulo, utilizando as fórmulas P = a + b + c,
+    A = (b * h ) / 2, onde a, b e c são os lados do triângulo
+    e h é a altura relativa ao lado b.
+*/
---
+++ 11_todo.cpp
@@ -0,0 +1,5 @@
+/*
+    Escreva um programa que calcule a velocidade média de um
+    objeto, utilizando a fórmula v = ds/dt, onde "v" é a velocidade
+    média, "ds" é a variação de espaço e "dt" variação de tempo.
+*/
