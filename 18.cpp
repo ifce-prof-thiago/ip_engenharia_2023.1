b4752987d40e082be9e80402c16991f0bc5026b6
Thiago Bandeira <thiago@lar.ifce.edu.br>
2023-06-22 10:25:52 -0300

    18.cpp

--- 18.cpp
+++ 18.cpp
@@ -1,4 +1,31 @@
 /*
-    Crie um programa que receba uma frase e substitua todas as letras
-    "a" por "e".
+    Crie um programa que receba uma frase e substitua
+    todas as letras "a" por "e".
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string frase;
+    cout << "Digite uma frase:";
+    getline(cin, frase);
+
+    cout << frase.size() << endl;
+
+    for (int i = 0; i < frase.size(); i++)
+    {
+        if (frase[i] == 'a')
+        {
+            frase[i] = 'e';
+        }
+
+        if (frase[i] == 'A')
+        {
+            frase[i] = 'E';
+        }
+    }
+
+    cout << "NOVA FRASE: " << frase << endl;
+}
